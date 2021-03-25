2b62c411961e569dee58401e083fe0449991889b
Isupov Vyacheslav <isupov@live.com>
2021-03-25 15:47:49 +0500

    2

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+let user = {
+  // объект
+  name: "John", // под ключом "name" хранится значение "John"
+  age: 30, // под ключом "age" хранится значение 30
+};
+// получаем свойства объекта:
+alert(user.name); // John
+alert(user.age); // 30
